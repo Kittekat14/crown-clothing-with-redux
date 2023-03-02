823c2a73d8d02e59f2e65e9dc7a9b8260cfd5f7f
Kittekat14 <katihofstetter@gmail.com>
2023-03-02 16:16:28 +0100

    adding redux dev-tools

--- src/store/store.js
+++ src/store/store.js
@@ -7,23 +7,9 @@ import {
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // will use localStorage by default in every browser
 
-// import logger from "redux-logger";
+import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
-
-const ourOwnLoggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log("action type:", action.type);
-  console.log("action payload:", action.payload);
-  console.log("currentState:", store.getState());
-
-  next(action);
-
-  console.log("next state: ", store.getState());
-};
 
 const persistConfig = {
   key: "root",
@@ -34,10 +20,18 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // middlewares ~ library helpers run before the action hits the reducer
-const middleWares = [ourOwnLoggerMiddleware];
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
+);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 // enhancer, because we enhance our store + compose: composes functions left to right when having more than 1 middleware
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 // root-reducer
 export const store = createStore(
